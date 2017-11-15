d4717072be760bf2ddb3ae0f883571e0ef197f4d
David Akre <dakre21@gmail.com>
2017-11-14 21:04:53 -0700

    initial push

---
+++ cuda_ml/__main__.py
@@ -0,0 +1,18 @@
+'''
+Author: David Akre
+Date: 11/14/17
+Description: Main entry point for cuda ml application
+'''
+
+import sys
+import pycuda.driver as cuda
+import pycuda.autoinit
+from pycuda.compiler import SourceModule
+
+def main():
+    print("****************************")
+    print("Starting CUDA ML Application")
+    print("****************************")
+
+if __name__ == "__main__":
+    main()
